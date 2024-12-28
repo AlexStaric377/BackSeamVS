de42c3ff5a30e2f884f5a8bcf8fe50666c1b0c8b
Alex Staric Seam <staric377@gmail.com>
2024-12-28 18:59:58 +0200

    Update NsiDetailing(

--- ViewModelAnalogDiagnoz.cs
+++ ViewModelAnalogDiagnoz.cs
@@ -264,7 +264,6 @@ namespace BackSeam
                   (readColectionIntreview = new RelayCommand(obj =>
                   {
                       MapOpisViewModel.IndexAddEdit = "";
-                      //MapOpisViewModel.ModelCall = "ModelColectionInterview";
                       MapOpisViewModel.GetidkodProtokola = KodProtokola;
 
                       WinCreatIntreview NewOrder = new WinCreatIntreview();
